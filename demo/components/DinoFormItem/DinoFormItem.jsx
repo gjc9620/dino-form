91606a1dbed5a1e87f2188a82c0fb2627613dbae
gjc9620 <992758355@qq.com>
2019-01-17 21:07:56 +0800

    sync value

--- demo/components/DinoFormItem/DinoFormItem.jsx
+++ demo/components/DinoFormItem/DinoFormItem.jsx
@@ -36,13 +36,32 @@ class DinoFormItem extends React.Component {
   
     store.remove(field);
   }
-  onChange = (value)=>{
+  isEventObj = (obj) =>{
+    if(typeof obj !== 'object'){
+      return false
+    }
+    
+    return (
+      obj.type !== undefined &&
+      obj.eventPhase !== undefined &&
+      obj.target !== undefined &&
+      typeof obj.stopPropagation === 'function' &&
+      typeof obj.preventDefault === 'function'
+    )
+  }
+  getValueFromEvent(e){
+    const { target } = e;
+    return target.type === 'checkbox' ? target.checked : target.value;
+  }
+  onChange = (arg)=>{
     const {
       dinoForm: {
         store,
       },
       field,
     } = this.props;
+    
+    const value = this.isEventObj(arg)? this.getValueFromEvent(arg): args;
   
     const scheme = store.get(field);
     
