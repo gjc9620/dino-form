5350fc5eaa8a133226fa8bc467725c5e542d8208
gjc9620 <992758355@qq.com>
2019-01-31 14:54:05 +0800

    export util

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ export { default as DinoFormItem } from './DinoFormItem';
 export { default as DinoFormStore } from './DinoFormStore';
 export * from './DinoFormHelper';
 export { default as dinoFromItemify } from './dinoFromItemify';
+export * from './util';
 
 export default createForm;
