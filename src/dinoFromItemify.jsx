603575f811d49e780a6a3785915c720a41611ba7
gjc9620 <992758355@qq.com>
2019-02-02 17:08:24 +0800

    remove comInitProps

--- src/dinoFromItemify.jsx
+++ src/dinoFromItemify.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { isEventObj, getValueFromEvent } from './util';
 
-export default function dinoFromItemify(Com) {
+export default function dinoFromItemify(Com, fromItemInitProps = {}, comInitProps = {}) {
   return class DinoComponent extends React.Component {
     // constructor(props) {
     //   super(props);
