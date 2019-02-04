092840d85500bcea4f2feaef74896af543800bcc
gjc9620 <992758355@qq.com>
2019-02-05 00:11:12 +0800

    eslint

--- src/Drag.jsx
+++ src/Drag.jsx
@@ -70,29 +70,35 @@ class App extends Component {
     return (
       <DragDropContext onDragEnd={ this.onDragEnd }>
         <Droppable droppableId="droppable">
-          {(provided, snapshot) => (
-            <div
-              ref={ provided.innerRef }
-              style={ getListStyle(snapshot.isDraggingOver) }>
-              {this.state.items.map((item, index) => (
-                <Draggable key={ item.id } draggableId={ item.id } index={ index }>
-                  {(provided, snapshot) => (
-                    <div
-                      ref={ provided.innerRef }
-                      { ...provided.draggableProps }
-                      { ...provided.dragHandleProps }
-                      style={ getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style,
-                      ) }>
-                      {item.content}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
+          {
+            (provided, snapshot) => (
+              <div
+                ref={ provided.innerRef }
+                style={ getListStyle(snapshot.isDraggingOver) }>
+
+                {
+                  this.state.items.map((item, index) => (
+                    <Draggable key={ item.id } draggableId={ item.id } index={ index }>
+                      {(provided, snapshot) => (
+                        <div
+                          ref={ provided.innerRef }
+                          { ...provided.draggableProps }
+                          { ...provided.dragHandleProps }
+                          style={ getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style,
+                          ) }>
+                          {item.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))
+                }
+
+                {provided.placeholder}
+              </div>
+            )
+          }
         </Droppable>
       </DragDropContext>
     );
